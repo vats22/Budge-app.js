dbe246d435449f4a1ce858e091e0e1ca3a8da677
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2020-09-23 20:31:33 +0530

    setting up  event listener

--- app.js
+++ app.js
@@ -1,28 +1,35 @@
+// BUDGE CONTROLLER
 var budgetController = (function() {
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+   // some code
 })();
 
+// UI CONTROLLER
 var UIController = (function() {
     // some code
 })();
 
+// GLOBEL APP CONTROLLER
 var Controller = (function(budgectrl,UICtlrl) {
-    var z = budgectrl.publicTest(5);
 
-    return{
-        anotherPublic: function() {
-            console.log(z);
-        }
+    var ctlrAddItem = function() {
+        // 1. get the filed input data
+
+       // 2. add the item to  the budget controller
+
+       // 3. add item to the UI
+
+       // 4. Calculate the budget
+
+       // 5. Display the budget on the UI
+
+       console.log('yes u can  fuck')
     }
+    document.querySelector('.add__btn').addEventListener('click', ctlrAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+        ctlrAddItem();
+        }
+    });
 
 })(budgetController,UIController);
