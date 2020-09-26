d3e1f620cde43537c656b59d944fb5cda2a6131b
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2020-09-26 12:49:07 +0530

    adding item in budgetCtlr

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // BUDGE CONTROLLER
 var budgetController = (function() {
    
-    var Expnse = function(id,description,value) {
+    var Expense = function(id,description,value) {
         this.id = id;
         this.description = description;
         this.value = value;
@@ -18,11 +18,46 @@ var budgetController = (function() {
             exp:[],
             inc:[]
         },
-
         totals: {
             exp: 0,
             inc: 0
         }
+    };
+
+    return {
+
+        addItem: function(type,des,val) {
+
+            var newItem, ID;
+            
+            //[1 2 3 4 5], next ID = 6
+            //[1 2 4 6 8], next ID = 9
+            // ID = last ID + 1
+            
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            
+            // Return the new element
+            return newItem;
+        },
+        testing: function() {
+            console.log(data);
+        }
+        
     };
 })();
 
@@ -38,7 +73,7 @@ var UIController = (function() {
     return{
         getInput: function() {
             return {
-                type: document.querySelector(domString.inputType).value,
+                type: document.querySelector(domString.inputType).value, // it will be a inc or exp.
                 description: document.querySelector(domString.inputDescription).value,
                 value: document.querySelector(domString.inputValue).value
             };
@@ -63,15 +98,15 @@ var Controller = (function(budgectrl,UICtlrl) {
                 ctlrAddItem();
             }
         });
-    };
+    };  
     
 
     var ctlrAddItem = function() {
+        var input, newitem;
         // 1. get the filed input data
-        var input = UICtlrl.getInput();
-        console.log(input);
+            input = UICtlrl.getInput();
        // 2. add the item to  the budget controller
-
+            newitem = budgectrl.addItem(input.type, input.description, input.value);
        // 3. add item to the UI
 
        // 4. Calculate the budget
